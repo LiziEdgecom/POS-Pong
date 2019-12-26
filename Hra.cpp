866c5ee3f6ea921eba218f875fe4d3b1fd086850
Andrej Čačala <cacala@stud.uniza.sk>
2019-12-26 19:12:48 +0100

    vytvorenie tried

---
+++ Hra.cpp
@@ -0,0 +1,2 @@
+
+#include "Hra.h"
--- Hrac.h
+++ Hrac.h
@@ -1,13 +1,23 @@
 #pragma once
 #include <string>
+using namespace std;
 class Hrac {
 
 private:
 	int polohaX;
 	int polohaY;
+	int pociatocneX;
+	int pociatocneY;
 	int score;
 	string meno;
-
+	
 public:
-
+	Hrac(int x, int y, string paMeno);
+	~Hrac();
+	void PohybHore();
+	void PohybDole();
+	void Reset();
+	void Score();
 };
+
+
