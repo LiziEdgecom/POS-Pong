74696a8fe98c2f64d5b7ef28011c8c15de4a73bc
Andrej Čačala <cacala@stud.uniza.sk>
2020-01-05 19:11:54 +0100

    .

--- Main.cpp
+++ Main.cpp
@@ -9,20 +9,21 @@
 int main(){	
 	
 
-	/*Klient* klient = new Klient();
+	Klient* klient = new Klient();
 	if (!klient->getZlyhanie())
 	{
 		klient->hra();
-	}*/
+	}
 	
-	int sirka = 800;
-	int vyska = 640;
-	Hrac* hrac1 = new Hrac(2,315,"andrej");
-	Hrac* hrac2 = new Hrac(sirka-17,315,"matus");
-	Lopta* lopta = new Lopta(400, 320);
-	Hra* hra = new Hra(sirka, vyska, hrac1, hrac2, lopta);
-	hra->zobrazenie();	
-	//hra->vykreslenie();
+	//int sirka = 800;
+	//int vyska = 640;
+	//Hrac* hrac1 = new Hrac(2,315,"andrej");
+	//Hrac* hrac2 = new Hrac(sirka-17,315,"matus");
+	//Lopta* lopta = new Lopta(400, 320);
+	//Hra* hra = new Hra(sirka, vyska, hrac1, hrac2, lopta);
+	//hra->zobrazenie();	
+	////hra->vykreslenie();
 		
 	return 0;
+
 }
