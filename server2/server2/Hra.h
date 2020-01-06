ed50800efa6dc165dc2ae271dfb0ee71c8420df3
LiziEdgecom <59146970+LiziEdgecom@users.noreply.github.com>
2020-01-06 15:31:50 +0100

    Add files via upload
    seever update

---
+++ server2/server2/Hra.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Hrac.h"
+#include "Lopta.h"
+
+//#include <SFML/Graphics.hpp>
+
+
+
+class Hra
+{
+private:
+	int sirkaPola;
+	int vyskaPola;
+	Hrac* hrac1;
+	Hrac* hrac2;
+	Lopta* lopta;
+	bool koniec;
+public:
+	Hra(int sirka, int vyska, Hrac* player1, Hrac* player2, Lopta* paLopta);
+	~Hra();
+
+	void vykreslenie();
+	void zobrazenie();
+	void kolizie();
+	void vstupy();
+
+	int getSirka() { return sirkaPola; };
+	int getVyska() { return vyskaPola; };
+	bool getKoniec() { return koniec; };
+	Hrac* getHrac1() { return hrac1; };
+	Hrac* getHrac2() { return hrac2; };
+	Lopta* getLopta() { return lopta; };
+};
+
